8cd90b7eff1342d0fe0de65d82f2f9d7f1bb5b5d
川野勇也 <kawanoisamunari@kawanoisamunarinoMacBook-Pro.local>
2022-11-18 06:36:03 +0900

    戻した

--- src/api/main.go
+++ src/api/main.go
@@ -2,49 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
-	"encoding/json"
-	"net/http"
-	"strconv"
 )
 
-type(
-	SampleHandler struct{
-		Data string
-	}
-	SampleResponse struct{
-		Status	string`json:"status"`
-		Message  string`json:"message"`
-		StatusCode string`json:"statuscode"`
-	}
-)
+func main() {
+	router := gin.Default()
 
-func main(){
-	mux := &http.ServeMux{}
-	mux.Handle("/view",NewSampleHandler())
-	if err := http.ListenAndServe(":3000",mux);err != nil{
-		log.Fatal(err)
-	}
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	router.Run(":3000")
 }
-
-func NewSampleHandler()http.Handler{
-	return &SampleHandler{"Hello,World!"}
-}
-
-func(h*SampleHandler)ServeHTTP(w http.ResponseWriter,r*http.Request){
-	statuscode:=200
-
-	res:= &SampleResponse{
-		Status: "OK",
-		Message: h.Data,
-		StatusCode: strconv.Itoa(statuscode),
-	}
-
-	w.Header().Set("Content-Type","application/json; charset=UTF-8")
-
-	w.WriteHeader(statuscode)
-
-	buf,_:=json.MarshalIndent(res,""," ")
-	_,_=w.Write(buf)
-}
-
